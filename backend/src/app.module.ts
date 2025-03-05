10affa4e64ff62b0d2b6e10471bb34f144951b59
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2025-03-05 22:08:47 +0530

    add products module with CRUD operations; integrate Prisma for database management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, ProductsService],
 })
 export class AppModule {}
