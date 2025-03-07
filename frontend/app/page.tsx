607f5bb76c19d63127ce85acf71372150f4a9646
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2025-03-07 23:58:43 +0530

    add About page and integrate it into Home component; update Header positioning

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,13 @@
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import Header from "./components/header";
+import About from "./pages/about";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
---
+++ frontend/app/pages/about.tsx
@@ -0,0 +1,13 @@
+
+
+export default function about(){
+
+
+    return(
+        <>
+            <div>
+            <h1 className="text-4xl font-bold">About Me</h1>
+            </div>
+        </>
+    )
+}
