53dbf6d8bd0a400b1c9f215a1f208110b39d3dc2
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-07 17:25:08 +0530

    replace Loader component with Lottie animation; update loading state management in _app.tsx

--- frontend/components/loader.tsx
+++ frontend/components/loader.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
-import Anmation from '../assets/Loading-Animation.gif'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 
 
 export default function Loader() {
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-white/70 backdrop-blur-md">
-        <Image src={Anmation} className="w-24" alt="loading animation"/>
+        <div>
+            <DotLottieReact
+                src="https://lottie.host/29622401-58dd-4a9a-8096-e28007e794a6/HrqtuCl6qN.lottie"
+                loop
+                autoplay
+                />
+        </div>
       </div>
     );
   }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,16 +4,31 @@ import { HeroUIProvider } from "@heroui/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useRouter } from "next/router";
 
+import { useState, useEffect } from "react";
+import Loader from "@/components/loader";
+
 import { geistSans, geistMono } from "@/config/fonts";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <HeroUIProvider navigate={router.push}>
       <NextThemesProvider>
-        <Component className={`${geistSans.variable} ${geistMono.variable} antialiased`} {...pageProps} />
+      <Component {...pageProps} />
       </NextThemesProvider>
     </HeroUIProvider>
   );
