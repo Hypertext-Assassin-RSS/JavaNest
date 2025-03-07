5bf2e8a81c6587b3738a89824041b48e3f5d0835
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-07 12:11:59 +0530

    add Auth module with JWT authentication; implement user registration and login functionality

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,9 +13,10 @@ import { OrderModule } from './order/order.module';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, S3Module, OrderModule, UserModule],
+  imports: [ProductModule, S3Module, OrderModule, UserModule, AuthModule],
   controllers: [AppController, ProductController, OrdersController, UserController],
   providers: [AppService, PrismaService, ProductService, S3Service, OrdersService, UserService],
 })
