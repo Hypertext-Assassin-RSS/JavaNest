e0cd8fe0a9e96c46d71f635f8a61d69da6b868e0
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-10 17:23:22 +0530

    feat: add background image to About page for enhanced visual appeal

--- frontend/app/pages/about.tsx
+++ frontend/app/pages/about.tsx
@@ -1,18 +1,24 @@
 import Image from 'next/image'
 import AboutLogo from '../../assets/coffee-cup.png'
+import Background from '../../assets/cffee-background.png'
 
 
 export default function About(){
 
 
     return (
-        <section className="flex flex-col items-center justify-evenly lg:flex-row min-h-screen">
+        <section className="flex flex-col items-center justify-evenly lg:flex-row min-h-screen" style={{ 
+          backgroundImage: `url(${Background.src})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}>
           <div className='flex flex-col items-center justify-evenly'>
             <h1 className="text-4xl font-bold">About Us</h1>
-            <span className="text-xl font-semibold text-gray-700 dark:text-gray-500 mt-2">
+            <span className="text-xl font-semibold text-gray-700 mt-2">
               Enjoy Your Day With Refreshing Coffee From
             </span>
-            <span className="text-xl font-semibold text-gray-700 dark:text-gray-500 mt-0">
+            <span className="text-xl font-semibold text-gray-700 mt-0">
             JavaNest
             </span>
           </div>
