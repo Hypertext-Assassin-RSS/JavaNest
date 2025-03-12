bc899f88dab270b3616226b351472d3993697074
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2025-03-12 21:55:47 +0530

    feat: add AboutUs page and integrate it into the Home component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,12 +5,14 @@ import { useTheme } from "next-themes";
 import Header from "./components/header";
 import About from "./pages/about";
 import Products from "./pages/coffeeMenu";
+import AboutUs from "./pages/aboutUs";
 
 export default function Home() {
   return (
     <>
       <Header />
       <About />
+      <AboutUs />
       <Products />
     </>
   );
