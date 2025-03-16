99a0210c732ccd02cdf030dd33f10082e5c987bc
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2025-03-16 12:37:23 +0530

    feat: implement Table and Booking modules with services and controllers; add database migrations and update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,10 +14,16 @@ import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { BookingService } from './booking/booking.service';
+import { BookingController } from './booking/booking.controller';
+import { BookingModule } from './booking/booking.module';
+import { TableController } from './table/table.controller';
+import { TableService } from './table/table.service';
+import { TableModule } from './table/table.module';
 
 @Module({
-  imports: [ProductModule, S3Module, OrderModule, UserModule, AuthModule],
-  controllers: [AppController, ProductController, OrdersController, UserController],
-  providers: [AppService, PrismaService, ProductService, S3Service, OrdersService, UserService],
+  imports: [ProductModule, S3Module, OrderModule, UserModule, AuthModule, BookingModule, TableModule],
+  controllers: [AppController, ProductController, OrdersController, UserController, BookingController, TableController],
+  providers: [AppService, PrismaService, ProductService, S3Service, OrdersService, UserService, BookingService, TableService],
 })
 export class AppModule {}
