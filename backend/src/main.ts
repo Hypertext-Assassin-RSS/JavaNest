78efa7bba7b09858668364af64f60a7c506a1699
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2025-03-19 19:59:50 +0530

    feat: add Swagger documentation setup to the NestJS application

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('JavaNest API')
+    .setDescription('API documentation for the JavaNest')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
